7979894f403304da8e81053c48dea81c419f4703
atrox3d <atrox3d@gmail.com>
2024-09-16 19:11:18 +0200

    format views and html

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data = {
+        'movies': [
+            {
+                'id': 5,
+                'title': 'Jaws',
+                'year': 1969
+            },
+            {
+                'id': 6,
+                'title': 'sharknado',
+                'year': 1900
+            },
+            {
+                'id': 7,
+                'title': 'The Meg',
+                'year': 1980
+            },
+        ]
+    }
 
 def movies(request):
     # return HttpResponse("hello there")
-    data = {
-            'movies': [
-                {
-                    'id': 5,
-                    'title': 'Jaws',
-                    'year': 1969
-                },
-                {
-                    'id': 6,
-                    'title': 'sharknado',
-                    'year': 1900
-                },
-                {
-                    'id': 7,
-                    'title': 'The Meg',
-                    'year': 1980
-                },
-            ]
-        }
 
     return render(
         request, 
