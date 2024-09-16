75c929b05bc0430b77e2d84b81b5fa2f65417b2a
atrox3d <atrox3d@gmail.com>
2024-09-16 18:59:07 +0200

    add data, html

--- movies/views.py
+++ movies/views.py
@@ -5,7 +5,23 @@ from django.shortcuts import render
 def movies(request):
     # return HttpResponse("hello there")
     data = {
-            'movies': ['movie1', 'movie2']
+            'movies': [
+                {
+                    'id': 5,
+                    'title': 'Jaws',
+                    'year': 1969
+                },
+                {
+                    'id': 6,
+                    'title': 'sharknado',
+                    'year': 1900
+                },
+                {
+                    'id': 7,
+                    'title': 'The Meg',
+                    'year': 1980
+                },
+            ]
         }
 
     return render(
