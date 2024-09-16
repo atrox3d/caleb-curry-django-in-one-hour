3c2d78fbb84305b193f27a641509ded86c5773ac
atrox3d <atrox3d@gmail.com>
2024-09-16 18:24:52 +0200

    add movies template

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def movies(request):
-    return HttpResponse("hello there")
+    # return HttpResponse("hello there")
+    return render(
+        request, 
+        'movies/movies.html', {
+            'movies': ['movie1', 'movie2']
+        }
+    )
 
 def home(request):
     return HttpResponse("home page")
