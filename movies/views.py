6566cedeca93cbb099798524f043f1ddf63852ed
atrox3d <atrox3d@gmail.com>
2024-09-17 10:02:13 +0200

    TypeError: context must be a dict rather than a QuerySet

--- movies/views.py
+++ movies/views.py
@@ -1,29 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-data = {
-        'movies': [
-            {
-                'id': 5,
-                'title': 'Jaws',
-                'year': 1969
-            },
-            {
-                'id': 6,
-                'title': 'sharknado',
-                'year': 1900
-            },
-            {
-                'id': 7,
-                'title': 'The Meg',
-                'year': 1980
-            },
-        ]
-    }
+from .models import Movie
+data = { 'movies': [ { 'id': 5, 'title': 'Jaws', 'year': 1969 }, { 'id': 6, 'title': 'sharknado', 'year': 1900 }, { 'id': 7, 'title': 'The Meg', 'year': 1980 }, ] }
 
 def movies(request):
     # return HttpResponse("hello there")
-
+    data = Movie.objects.all()
     return render(
         request, 
         'movies/movies.html', data
