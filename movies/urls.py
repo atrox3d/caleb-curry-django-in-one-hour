af5d3ee44f405149e9938ff64acae3147729ee21
atrox3d <atrox3d@gmail.com>
2024-09-17 10:23:56 +0200

    add movie detail view

--- movies/urls.py
+++ movies/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', views.movies),
     path('', views.home),
+    path('movies/<int:id>', views.detail)
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie
-data = { 'movies': [ { 'id': 5, 'title': 'Jaws', 'year': 1969 }, { 'id': 6, 'title': 'sharknado', 'year': 1900 }, { 'id': 7, 'title': 'The Meg', 'year': 1980 }, ] }
+
+# data = { 'movies': [ { 'id': 5, 'title': 'Jaws', 'year': 1969 }, { 'id': 6, 'title': 'sharknado', 'year': 1900 }, { 'id': 7, 'title': 'The Meg', 'year': 1980 }, ] }
 
 def movies(request):
     # return HttpResponse("hello there")
     data = Movie.objects.all()
     return render(
         request, 
-        'movies/movies.html', {'movies': data}
+        'movies/movies.html', 
+        {'movies': data}
+    )
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(
+        request,
+        'movies/detail.html',
+        {'movie': data}
     )
 
 def home(request):
