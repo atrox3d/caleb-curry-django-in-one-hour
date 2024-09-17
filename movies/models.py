a04e63055d1a93bfeddf21b135605c4d284cd254
atrox3d <atrox3d@gmail.com>
2024-09-17 09:59:34 +0200

    add Movie.__str__()

--- movies/models.py
+++ movies/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     year = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.title} ({self.year})'
