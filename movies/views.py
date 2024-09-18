fd586400d24227d65dd09dc67c44fd3a47d66638
atrox3d <atrox3d@gmail.com>
2024-09-18 06:30:20 +0200

    add movie

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
+from turtle import title
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Movie
 
 # data = { 'movies': [ { 'id': 5, 'title': 'Jaws', 'year': 1969 }, { 'id': 6, 'title': 'sharknado', 'year': 1900 }, { 'id': 7, 'title': 'The Meg', 'year': 1980 }, ] }
+
+def home(request):
+    return HttpResponse("home page")
 
 def movies(request):
     # return HttpResponse("hello there")
@@ -21,5 +25,16 @@ def detail(request, id):
         {'movie': data}
     )
 
-def home(request):
-    return HttpResponse("home page")
+def add(request:HttpRequest):
+
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if all(x is not None for x in (title, year)):
+        Movie(title=title, year=year).save()
+        return HttpResponseRedirect('/movies')
+
+    return render(
+        request,
+        'movies/add.html'
+    )
